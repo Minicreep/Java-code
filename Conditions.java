4ec3bf396b7e5178d7633e16b3654eb007a23275
Arjun Sharawat <arjunsharawath37@gmail.com>
2025-10-19 20:00:00 +0530

    Add files via upload

---
+++ Conditions.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Conditions {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter the number:");
+		int num=sc.nextInt();
+		
+		if(num>0)
+			System.out.println("the number is positive");
+		else if(num<0)
+			System.out.println("the number is negative");
+		else
+			System.out.println("the number is Zero");
+		
+		
+		System.out.println("Enter the number(even/odd):");
+		int n=sc.nextInt();
+		
+		if(n != 0)
+			if(n%2==0)
+				System.out.println("the number is even");
+			else
+				System.out.println("the number is odd");
+		
+		sc.close();
+		
+	}
+
+}
